6250a59383bd35db6a7bd849a51d6c38445c5191
yuriy.mametskiy <yuriy.mametskiy@dukascopy.com>
2023-08-29 23:16:06 +0300

    nav all

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-function App() {
-    return (
+import { Routes, Route, Navigate } from "react-router-dom";
+import { ROUTES } from "./const";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Link } from "react-router-dom";
 
-    <div className="App">
-        lorem ipsum dolor sit amet
-    </div>
+
+
+export const App = () => {
+const {home, profile} = ROUTES;
+
+    return (
+      <>
+      <header>
+        <Link to="/home">Go to Home</Link>
+        &nbsp;|&nbsp;
+        <Link to="/profile">Go to Profile</Link>
+      </header>
+
+      <Routes>
+        <Route path={home} element={<Home />} />
+        <Route path={profile} element={<Profile />} />
+        <Route path="/" element={<Navigate to={home} />} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+      </>
+      
     )
 }
-
-export default App;
